caf09c9ee8c55108898676791abc69840181c920
sseba20h <sseba20h@gmail.com>
2024-04-26 22:24:59 +0200

    123

--- Dictionary.py
+++ Dictionary.py
@@ -79,17 +79,17 @@ for x in range(1, n + 1):
 
 d = dict()
 for x in range(1, 16):
-    d[x] = x ** 2
+#    d[x] = x ** 2
 
 #print(d)
 
 #8. Write a Python script to merge two Python dictionaries.
 
-d1 = {'a': 100, 'b': 200}
-d2 = {'x': 300, 'y': 200}
-d = d1.copy()
+#d1 = {'a': 100, 'b': 200}
+#d2 = {'x': 300, 'y': 200}
+#d = d1.copy()
 
-d.update(d2)
+#d.update(d2)
 
 #print(d)
 
@@ -104,4 +104,14 @@ d.update(d2)
 
 my_dict = {'data1': 100, 'data2': -54, 'data3': 247}
 result = sum(my_dict.values())
+#print(result)
+
+#11. Write a Python program to multiply all the items in a dictionary.
+
+my_dict = {'data1': 100, 'data2': -54, 'data3': 247}
+result = 1
+
+for key in my_dict:
+    result = result * my_dict[key]
+
 print(result) 
