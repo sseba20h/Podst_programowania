0c928bfcec2ac3e526ff32425fa51ae4bd444b82
sseba20h <sseba20h@gmail.com>
2024-04-26 22:45:26 +0200

    Update Statements and loops.py

---
+++ Statements and loops.py
@@ -0,0 +1,39 @@
+#1. Write a Python program to find those numbers which are divisible by 7 and multiples of 5, between 1500 and 2700 (both included).
+
+#2. Write a Python program to convert temperatures to and from Celsius and Fahrenheit.
+
+#3. Write a Python program to guess a number between 1 and 9.
+
+#4. Write a Python program to construct the following pattern, using a nested for loop.
+
+#5. Write a Python program that accepts a word from the user and reverses it.
+
+#6. Write a Python program to count the number of even and odd numbers in a series of numbers
+
+#7. Write a Python program that prints each item and its corresponding type from the following list.
+
+#8. Write a Python program that prints all the numbers from 0 to 6 except 3 and 6.
+
+#9. Write a Python program to get the Fibonacci series between 0 and 50.
+
+#10. Write a Python program that iterates the integers from 1 to 50.
+
+#11. Write a Python program that takes two digits m (row) and n (column) as input and generates a two-dimensional array.
+
+#12. Write a Python program that accepts a sequence of lines (blank line to terminate) as input and prints the lines as output (all characters in lower case).
+
+#13. Write a Python program that accepts a sequence of comma separated 4 digit binary numbers as its input.
+
+#14. Write a Python program that accepts a string and calculates the number of digits and letters.
+
+#15. Write a Python program to check the validity of passwords input by users.
+
+#16. Write a Python program to find numbers between 100 and 400 (both included) where each digit of a number is an even number.
+
+#17. Write a Python program to print the alphabet pattern 'A'.
+
+#18. Write a Python program to print the alphabet pattern 'D'.
+
+#19. Write a Python program to print the alphabet pattern 'E'.
+
+#20. Write a Python program to print the alphabet pattern 'G'.
