7be76223b809a73c8641d3e8f0a3100dd93d9c89
sseba20h <sseba20h@gmail.com>
2024-05-05 21:09:17 +0200

    Update wykłady1.py

--- wykłady1.py
+++ wykłady1.py
@@ -1,12 +1,1 @@
-a = input('Podaj pierwszą liczbę:')
-b = input('Podaj drugą liczbę:')
-
-# Convert the inputs to integers before doing addition
-a = int(a)
-b = int(b)
-
-# Perform addition of a and b
-c = a + b
-
-# Print the result
-print(str(a) + " + " + str(b) + " = " + str(c))
+print('Hello')
