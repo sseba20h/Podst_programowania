2f05e44147b8b007c1bdef0ec517d56da512d4ae
sseba20h <sseba20h@gmail.com>
2024-05-31 23:06:50 +0200

    Update Functions.py

--- Zad_5/Functions.py
+++ Zad_5/Functions.py
@@ -69,7 +69,7 @@ def distinct_elements(*args):
             seen[element] = True
     return result
 
-#print(distinct_elements(1 ,1 ,2 , 55, 55, 4, 2))
+print(distinct_elements(1 ,1 ,2 , 55, 55, 4, 2))
 
 def prime(*numbers):
     def is_prime(n):
@@ -213,26 +213,26 @@ def perfect_number(n):
 
 #print(perfect_number(6))
 
-def make_bold(fn):
-    def wrapped():
-        return "<b>" + fn() + "</b>"
-    return wrapped
+# def make_bold(fn):
+#     def wrapped():
+#         return "<b>" + fn() + "</b>"
+#     return wrapped
 
-def make_italic(fn):
-    def wrapped():
-        return "<i>" + fn() + "</i>"
-    return wrapped
+# def make_italic(fn):
+#     def wrapped():
+#         return "<i>" + fn() + "</i>"
+#     return wrapped
 
-def make_underline(fn):
-    def wrapped():
-        return "<u>" + fn() + "</u>"
-    return wrapped
+# def make_underline(fn):
+#     def wrapped():
+#         return "<u>" + fn() + "</u>"
+#     return wrapped
 
-@make_bold
-@make_italic
-@make_underline
-def hello():
-    return "hello world"
+# @make_bold
+# @make_italic
+# @make_underline
+# def hello():
+#     return "hello world"
 
 
-print(hello())
+# print(hello())
