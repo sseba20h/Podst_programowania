01bacf1015f305880e8a27fbd637c019f8b3ee5d
sseba20h <sseba20h@gmail.com>
2024-06-01 22:30:52 +0200

    Update Class.py

--- Zad_5/Class.py
+++ Zad_5/Class.py
@@ -1,29 +1,121 @@
 #1
-import array
-for name in array.__dict__:
-    print(name)
+# import array
+# for name in array.__dict__:
+#     print(name)
 #2
-
+# class py_solution:
+#     def sub_sets(self, sset):
+#         return self.subsetsRecur([], sorted(sset))    
+#     def subsetsRecur(self, current, sset):
+#         if sset:
+#             return self.subsetsRecur(current, sset[1:]) + self.subsetsRecur(current + [sset[0]], sset[1:])
+#         return [current]
+# for name in py_solution.__dict__:
+#     print(name)
 #3
-
+# class Student: 
+#     def __init__(self, student_id, student_name, class_name):
+#         self.student_id = student_id
+#         self.student_name = student_name
+#         self.class_name = class_name 
+# student = Student('V12', 'Frank Gibson', 'V')
+# print(student.__dict__)
 #4
-
+# import builtins 
+# help(builtins.abs)
+# print(builtins.abs(-155))
 #5
-
+# def student(student_id, student_name, student_class):
+#     return f'Student ID: {student_id}\nStudent Name: {student_name}\nClass: {student_class}'
+# print(student('S122', 'Wilson Medina', 'VI'))
 #6
+# def student_data(student_id, **kwargs):
+#     print(f'\nStudent ID: {student_id}')
+#     if 'student_name' in kwargs:
+#         print(f"Student Name: $ {kwargs['student_name']}")
+    
+#     if 'student_name' and 'student_class' in kwargs:
+#             print(f"\nStudent Name: $ {kwargs['student_name']}")
+#             print(f"Student Class: $ {kwargs['student_class']}")
 
+ 
+# student_data(student_id='SV12', student_name='Jean Garner')
+
+# student_data(student_id='SV12', student_name='Jean Garner', student_class ='V')
 #7
-
+# class Student:
+#     pass  
+# print(type(Student))
+# print(Student.__dict__.keys())
+# print(Student.__module__)
 #8
-
+# class Student:
+#     pass 
+# class Marks:
+#     pass 
+# student1 = Student()
+# marks1 = Marks()
+# print(isinstance(student1, Student))
+# print(isinstance(marks1, Student))
+# print(isinstance(marks1, Marks)) 
+# print(isinstance(student1, Marks))
+# print("\nCheck whether the said classes are subclasses of the built-in object class or not.")
+# print(issubclass(Student, object))
+# print(issubclass(Marks, object)) 
 #9
-
+# class Student:
+#     student_name = 'Terrance Morales'
+#     marks = 93  
+# print(f"Student Name: {getattr(Student, 'student_name')}")
+# print(f"Marks: {getattr(Student, 'marks')}")
+# setattr(Student, 'student_name', 'Angel Brooks')
+# setattr(Student, 'marks', 95) 
+# print(f"Student Name: {getattr(Student, 'student_name')}")
+# print(f"Marks: {getattr(Student, 'marks')}")
 #10
-
+# class Student:
+#     student_id = 'V10'
+#     student_name = 'Jacqueline Barnett'  
+# print("Original attributes and their values of the Student class:")
+# for attr, value in Student.__dict__.items():
+#     if not attr.startswith('_'):
+#         print(f'{attr} -> {value}')
+# print("\nAfter adding the student_class, attributes and their values with the said class:")
+# Student.student_class  = 'V'
+# for attr, value in Student.__dict__.items():
+#     if not attr.startswith('_'):
+#         print(f'{attr} -> {value}')
+# print("\nAfter removing the student_name, attributes and their values from the said class:")
+# del Student.student_name
+# #delattr(Student, 'student_name')
+# for attr, value in Student.__dict__.items():
+#     if not attr.startswith('_'):
+#         print(f'{attr} -> {value}')
 #11
-
+# class Student:
+#     student_id = 'V10'
+#     student_name = 'Jacqueline Barnett'
+#     def display():
+#         print(f'Student id: {Student.student_id}\nStudent Name: {Student.student_name}')
+# print("Original attributes and their values of the Student class:")
+# Student.display()
 #12
-
+# class Student:
+#     school = 'Forward Thinking'
+#     address = '2626/Z Overlook Drive, COLUMBUS' 
+# student1 = Student()
+# student2 = Student() 
+# student1.student_id = "V12"
+# student1.student_name = "Ernesto Mendez"
+# student2.student_id = "V12"
+# student2.marks_language = 85
+# student2.marks_science = 93
+# student2.marks_math = 95 
+# students = [student1, student2]
+# for student in students:
+#     print('\n')
+#     for attr in student.__dict__:
+#         print(f'{attr} -> {getattr(student, attr)}')
 #1
 
 #2
